2de90af9be6a7091ec884ff5594d492f116f5c2c
manasa-annavaram <mannava1@terpmail.umd.edu>
2025-11-17 16:19:59 -0500

    Create wifi wireless Code.py

---
+++ wifi wireless Code.py
@@ -0,0 +1,13 @@
+from enes100 import enes100
+enes100.begin("Chris Morris AWOG",'Material', 402,1120)
+enes100.x
+enes100.y
+enes100.theta
+enes100.is_visible
+enes100.is_connected()
+enes100.print("Hi we are team chris morris AWOG")
+enes100.mission('WEIGHT', 'HEAVY')
+enes100.mission('WEIGHT', 'MEDIUM')
+enes100.mission('WEIGHT', 'LIGHT')
+enes100.mission('MATERIAL_TYPE', 'FOAM')
+enes100.mission('MATERIAL_TYPE', 'PLASTIC')
