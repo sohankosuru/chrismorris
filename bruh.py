08b9f2b4dc8c300b898ace8bc49253d9d42966a7
manasa-annavaram <mannava1@terpmail.umd.edu>
2025-11-20 17:36:59 -0500

    Update bruh.py

--- bruh.py
+++ bruh.py
@@ -13,10 +13,14 @@ pwm2 = PWM(pin25, freq=5000, duty_u16=32768)
 pwm3 = PWM(pin26, freq=5000, duty_u16=32768)
 
 while True:
+    
     pwm0.duty(1023)
     pwm2.duty(0)
-    
-    
-    
     pwm1.duty(0)
     pwm3.duty(1023)
+def forward():
+    pwm0.duty(1023)
+    pwm2.duty(0)
+    pwm1.duty(0)
+    pwm3.duty(1023)
+    
